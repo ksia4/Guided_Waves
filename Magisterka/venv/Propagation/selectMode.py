731235950738d7441c58f43dc722ec42d334b5d7
ksia4 <ksia4@onet.eu>
2018-08-07 20:24:36 +0200

    poprawki czwartego rozdziału

--- Magisterka/venv/Propagation/selectMode.py
+++ Magisterka/venv/Propagation/selectMode.py
@@ -280,8 +280,8 @@ class SelectedMode:
             plt.plot(dziady, self.k_v, markersize=3)
         plt.xlabel("Frequency [kHz]", fontsize=15)
         plt.ylabel("Wavenumber [rad/m]", fontsize=15)
-        plt.xlim([0, 180])#600
-        plt.ylim([0, 100])#2000
+        plt.xlim([0, 100])#600
+        plt.ylim([0, 400])#2000
         plt.xticks(size=13)
         plt.yticks(size=13)
         plt.show()
